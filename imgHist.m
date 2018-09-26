5934e20c93969c320ea0dff048be2a842dc804cb
Matías Cadaval <matias.cadaval@gmail.com>
2018-09-26 00:58:26 -0300

    Fix para imgHist

--- imgHist.m
+++ imgHist.m
@@ -1,11 +1,11 @@
 function f = imgHist(img)
 
 [x,y] = size(img);
-counter = zeros(1,255);
+counter = zeros(1,256);
 
 for i=1: x
     for j=1:y
-        counter(1,img(i,j)) = counter(1,img(i,j)) + 1;
+        counter(1,img(i,j)+1) = counter(1,img(i,j)+1) + 1;
     end
 end
 
