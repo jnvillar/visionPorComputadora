b02687f00a3c94129b752ba71f0d7d226e40e274
Matías Cadaval <matias.cadaval@gmail.com>
2018-09-27 19:30:46 -0300

    Agrego ejercicios que faltan en practica_1

--- practica_1.m
+++ practica_1.m
@@ -1,46 +1,62 @@
 img1 = imread('imgs/color.png');
 img2 = imread('imgs/color.png');
 bw = imread('imgs/b&w.png');
+% Poner show = 1 para mostrar imprimir en pantalla todas las imagenes
 show = 0;
 
 % Ej 1
 
 sum = imgSum(img1,img2);
-if(show), figure(1),title("Suma imagen"),imshow(sum), end
+if(show), figure('Name', 'Suma imagen'), imshow([img1, img2, sum]), end
 
 sub = imgSub(img1,img2);
-if(show), figure(2),title("Resta imagen"),imshow(sub) , end
+if(show), figure('Name', 'Resta imagen'), imshow([img1, img2, sub]), end
 
 prod = imgProd(img1,img2);
-if(show), figure(3),title("Prod imagen"),imshow(prod)  , end
+if(show), figure('Name', 'Producto imagen'), imshow([img1, img2, prod]), end
 
 scalar = imgScalarProduct(img1,3);
-if(show), figure(4),title("Producto escalar imagen"),imshow(scalar), end
+if(show), figure('Name', 'Producto escalar imagen'), imshow([img1, scalar]), end
 
 compress = imgDynamicRangeCompr(img1);
-if(show), figure(5),title("Compresion del rango dinamico"),imshow(compress), end
+if(show), figure('Name', 'Compresion del rango dinamico'), imshow([img1, compress]), end
 
 % Ej 2
 
 neg = imgNeg(img1);
-if(show), figure(6),title("Imagen en negativo"),imshow(neg), end
+if(show), figure('Name', 'Imagen en negativo'), imshow([img1, neg]), end
 
 % Ej 3
 
 imgThresh = imgThresholding(bw, 100);
-if(show), figure(7),title("Imagen con umbral"),imshow(imgThresh), end
+if(show), figure('Name', 'Imagen con umbral'), imshow([bw, imgThresh]), end
 
 % Ej 4
 
 planes = cat(2, imgBitPlane(bw, 1), imgBitPlane(bw, 2), imgBitPlane(bw, 3), imgBitPlane(bw, 4), imgBitPlane(bw, 5), imgBitPlane(bw, 6), imgBitPlane(bw, 7), imgBitPlane(bw, 8));
 
-if(show), figure(8),title("Bit Planes de una imagen"),imshow(planes), end
+if(show), figure('Name', 'Bit Planes de una imagen'), imshow(planes), end
 
 % Ej 5
 
 vector = imgHist(bw);
-if(1), figure(9),title("Histograma"),bar(vector), end
+if(show), figure('Name', 'Histograma'), bar(vector), end
 
+% Ej 6
 
+imgStretching = imgHistStretching(bw);
+if(show), figure('Name', 'Aumento de contraste de una imagen'), imshow([bw, imgStretching]), end
 
+% Ej 7
 
+imgEqualization = imgHistEqualization(bw);
+if(show), figure('Name', 'Ecualización por histograma de una imagen'), imshow([bw, imgEqualization]), end
+
+% Ej 8
+
+imgEqualizationDoble = imgHistEqualization(imgEqualization);
+if(show), figure('Name', 'Ecualización por histograma aplicada 2 veces'), imshow([bw, imgEqualizationDoble]), end
+
+% Al aplicar por segunda vez la ecualización por histograma observamos que
+% la imagen no se modifica. Por lo tanto el histograma obtenido en ambos
+% casos el mismo, y por ende también la función de distribución acumulada.
