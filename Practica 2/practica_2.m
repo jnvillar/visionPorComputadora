bc2219aafa85105a20596dccece61285b18c33d7
ax-lew <axel.lew@hotmail.com>
2018-10-01 19:53:40 -0300

    refacto

--- Practica 2/practica_2.m
+++ Practica 2/practica_2.m
@@ -36,19 +36,6 @@ if(show), figure('Name','Show hsi planes'), imshow(separate_planes(img1)), end
 if(show), figure('Name','Show hsi planes'), imshow(separate_planes(img2)), end
 if(show), figure('Name','Show hsi planes'), imshow(separate_planes(img3)), end
 
-function res=imgChangeSaturation(img, f)
-    hsvImg = rgb2hsv(img);
-    hsvImg(:,:,2) = f(hsvImg(:,:,2));
-    hsvImg(hsvImg > 1) = 1;
-    res = uint8(hsv2rgb(hsvImg)*256);
-end
-
-
-
-function f=cuadratic_function()
-    f = @(vec) vec.^2;
-end
-
 
 
 
