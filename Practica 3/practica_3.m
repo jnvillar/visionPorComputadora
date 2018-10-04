5ef4e34e8d65575679a7523beacaf99215c4e5e4
ax-lew <axel.lew@hotmail.com>
2018-10-04 17:08:48 -0300

    add files

--- Practica 3/practica_3.m
+++ Practica 3/practica_3.m
@@ -1,28 +1,25 @@
 filepath = fileparts(mfilename('fullpath'));
 addpath(strcat(filepath, '/../imgs'));
-img1 = imread('imgs/river1.jpg');
-img2 = imread('imgs/fruit.png');
-img3 = imread('imgs/big.jpg');
-imgBW = imread('imgs/b&w.png');
-dif = imread('imgs/difuminada.png');
+test_img = imread('imgs/test.png');
+lena_img = imread('imgs/lena.png');
 show = 1;
 
 % Ejercicio 1
 
 % a
 box_blur_mask = ones(1,9).*(1/9);
-gaussian_blur_mask = [1,2,1; 2,4,2; 1,2,1].*(1/16);
-border_mask = [-1,-1,-1;-1,8,-1;-1,-1,-1];
+gaussian_blur_mask = [1,2,1,2,4,2,1,2,1].*(1/16);
+border_mask = [-1,-1,-1,-1,8,-1,-1,-1,-1];
 
-res=image_convolution(img3, box_blur_mask);
-res2=image_convolution(img3, gaussian_blur_mask);
-res3=image_convolution(img3, border_mask);
-
-figure;
-imshow([img3, res]);
+res=image_convolution(lena_img, box_blur_mask);
+res2=image_convolution(lena_img, gaussian_blur_mask);
+res3=image_convolution(lena_img, border_mask);
 
 figure;
-imshow([img3, res2]);
+imshow([lena_img, res]);
 
 figure;
-imshow([img3, res3]);
+imshow([lena_img, res2]);
+
+figure;
+imshow([lena_img, res3]);
