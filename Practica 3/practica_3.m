6214cb13b4ba1689276eb3c3c3630e3571f98343
ax-lew <axel.lew@hotmail.com>
2018-10-04 18:50:34 -0300

    ej 5

--- Practica 3/practica_3.m
+++ Practica 3/practica_3.m
@@ -2,7 +2,7 @@ filepath = fileparts(mfilename('fullpath'));
 addpath(strcat(filepath, '/../imgs'));
 test_img = imread('imgs/test.png');
 lena_img = imread('imgs/lena.png');
-show = 1;
+show = 0;
 
 % Ejercicio 1
 
@@ -43,3 +43,11 @@ img = lena_img;
 params = containers.Map({'sigma', 'size'}, {5, 30});
 res_unsharp = unsharp_masking(img, params);
 if(show), figure('Name', 'Image unsharp masking'), imshow([img, res_unsharp]), end
+
+
+
+
+% Ejercicio 5
+img = lena_img;
+res_median_fitler = median_filter(img, 3);
+if(1), figure('Name', 'Image median filter'), imshow([img, res_median_fitler]), end
