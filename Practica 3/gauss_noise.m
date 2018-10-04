d429f2b1b462d80ced347a6731353f3341a7df30
jnvillar <juaninolivillar@gmail.com>
2018-10-04 18:31:01 -0300

    ej 4

---
+++ Practica 3/gauss_noise.m
@@ -0,0 +1,24 @@
+function f=gauss_noise(img, white_limit, black_limit, mu, sigma)
+    [i,j] = size(img);
+    for i=1:i
+        for j=1:j
+            f(i,j)=add_noise(img(i,j),white_limit, black_limit, mu, sigma);
+        end
+    end
+end
+
+function f=add_noise(pixel,white_limit,black_limit,  mu, sigma)
+   r = normrnd(mu,sigma);
+   if r <= white_limit
+       f=0;
+       return
+   end
+   if r > white_limit && r<= black_limit
+      f=255;
+      return
+   end
+   if r>black_limit
+       f=pixel;
+       return
+   end
+end
--- Practica 3/practica_3.m
+++ Practica 3/practica_3.m
@@ -2,7 +2,7 @@ filepath = fileparts(mfilename('fullpath'));
 addpath(strcat(filepath, '/../imgs'));
 test_img = imread('imgs/test.png');
 lena_img = imread('imgs/lena.png');
-show = 1;
+show = 0;
 
 % Ejercicio 1
 
@@ -15,11 +15,17 @@ res=image_convolution(lena_img, box_blur_mask);
 res2=image_convolution(lena_img, gaussian_blur_mask);
 res3=image_convolution(lena_img, border_mask);
 
-figure;
-imshow([lena_img, res]);
 
-figure;
-imshow([lena_img, res2]);
+if(show), figure, imshow([lena_img, res]); end
+if(show), figure, imshow([lena_img, res2]); end
+if(show), figure, imshow([lena_img, res3]); end
 
-figure;
-imshow([lena_img, res3]);
+%Ejercicio 4
+white_limit = 0.3;
+black_limit = 0.6;
+mu = 0;
+sigma = 1;
+lena_gauss_noise = gauss_noise(lena_img,white_limit, black_limit,mu, sigma);
+
+if(1), figure, imshow([lena_img, lena_gauss_noise]); end
+
