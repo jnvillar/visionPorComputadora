5779356b4c3668f916b86cd0fc8f7a4d0f5213d5
Matías Cadaval <matias.cadaval@gmail.com>
2018-10-04 18:36:41 -0300

    Ej1 and ej2

--- Practica 3/practica_3.m
+++ Practica 3/practica_3.m
@@ -7,36 +7,37 @@ show = 1;
 % Ejercicio 1
 
 % a
-% box_blur_mask = ones(1,9).*(1/9);
-% gaussian_blur_mask = [1,2,1,2,4,2,1,2,1].*(1/16);
-% border_mask = [-1,-1,-1,-1,8,-1,-1,-1,-1];
-% 
-% res=image_convolution(lena_img, box_blur_mask);
-% res2=image_convolution(lena_img, gaussian_blur_mask);
-% res3=image_convolution(lena_img, border_mask);
-% 
-% figure;
-% imshow([lena_img, res]);
-% 
-% figure;
-% imshow([lena_img, res2]);
-% 
-% figure;
-% imshow([lena_img, res3]);
+low_pass_filter_3x3 = (1/9).*ones(3);
+low_pass_filter_5x5 = (1/25).*ones(5);
 
+low_pass_filter_3x3_img = image_convolution(lena_img, low_pass_filter_3x3);
+low_pass_filter_5x5_img = image_convolution(lena_img, low_pass_filter_5x5);
+if(show), figure('Name','Low pass filter (3x3 and 5x5)'), imshow([lena_img, low_pass_filter_3x3_img, low_pass_filter_5x5_img]), end
 
+%b
+duplicate_filter_3x3 = zeros(3);
+duplicate_filter_3x3(2,2) = 2;
+high_pass_filter_3x3 = duplicate_filter_3x3 - low_pass_filter_3x3;
 
+duplicate_filter_5x5 = zeros(5);
+duplicate_filter_5x5(3,3) = 2;
+high_pass_filter_5x5 = duplicate_filter_5x5 - low_pass_filter_5x5;
 
+high_pass_filter_3x3_img = image_convolution(lena_img, high_pass_filter_3x3);
+high_pass_filter_5x5_img = image_convolution(lena_img, high_pass_filter_5x5);
+if(show), figure('Name','High pass filter (3x3 and 5x5)'), imshow([lena_img, high_pass_filter_3x3_img, high_pass_filter_5x5_img]), end
 
+% Ejercicio 2
+% Lo implementamos en el ejercicio anterior para poder aplicar los filtros
+% pasa bajos y pasa altos (image_convolution)
 
 % Ejercicio 3
-%a
 
+%a
 img = lena_img;
 params = containers.Map({'sigma', 'size'}, {5, 30});
 res_smoothing = smoothing(img, params);
 if(show), figure('Name', 'Image smoothing'), imshow([img, res_smoothing]), end
-
 
 %b
 img = lena_img;
