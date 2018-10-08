c3e55d7a0a38ed409aabc462357085dd56ceeac0
jnvillar <juaninolivillar@gmail.com>
2018-10-08 18:33:40 -0300

    gauss fix

--- Practica 3/gauss_noise.m
+++ Practica 3/gauss_noise.m
@@ -1,6 +1,7 @@
 function f=gauss_noise(img, white_limit, black_limit, mu, sigma)
-    noise_img = generate_noise_image(img,white_limit, black_limit, mu, sigma);
-    f = imadd(img, noise_img);
+    %noise_img = generate_noise_image(img,white_limit, black_limit, mu, sigma);
+    %f = imadd(img, noise_img);
+    f = add_noise(img, white_limit, black_limit, mu, sigma);
 end
 
 function f=generate_noise_image(img,white_limit,black_limit,  mu, sigma)
@@ -23,28 +24,21 @@ function f=generate_noise_image(img,white_limit,black_limit,  mu, sigma)
 end
 
 
-function f=gauss_noise_original(img, white_limit, black_limit, mu, sigma)
-    [i,j] = size(img);
+function f=add_noise(img,white_limit,black_limit,  mu, sigma)
+   [i,j] = size(img);
     for i=1:i
         for j=1:j
-            f(i,j)=add_noise(img(i,j),white_limit, black_limit, mu, sigma);
+            r = normrnd(mu,sigma);
+            r = mu+(sigma-mu)*r;
+            if r <= white_limit
+                f(i,j)=0;
+            end
+            if r > white_limit && r<= black_limit
+                f(i,j)=img(i,j);
+            end
+            if r>black_limit
+                f(i,j)=255;
+            end
         end
     end
-end
-
-function f=add_noise(pixel,white_limit,black_limit,  mu, sigma)
-   r = normrnd(mu,sigma);
-   r = mu+(sigma-mu)*r;
-   if r <= white_limit
-       f=0;
-       return
-   end
-   if r > white_limit && r<= black_limit
-      f=pixel;
-      return
-   end
-   if r>black_limit
-       f=255;
-       return
-   end
 end
