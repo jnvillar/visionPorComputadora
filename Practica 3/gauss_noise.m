13d1a3e9ef454ce72ba185fec55a85575ce1cd2d
jnvillar <juaninolivillar@gmail.com>
2018-10-08 17:34:49 -0300

    gauss filter

--- Practica 3/gauss_noise.m
+++ Practica 3/gauss_noise.m
@@ -1,4 +1,32 @@
 function f=gauss_noise(img, white_limit, black_limit, mu, sigma)
+    f = imadd(img,generate_noise_image(img,white_limit, black_limit, mu, sigma));
+end
+
+function f=generate_noise_image(img,white_limit,black_limit,  mu, sigma)
+    [X,Y] = size(img);
+    r = normrnd(mu,sigma);
+    r = mu+(sigma-mu)*r;
+
+    for i=1:X
+        for j=1:Y
+            if r <= white_limit
+                f(i,j) = -255;
+            end
+            if r > white_limit && r<= black_limit
+                f(i,j) = 0;
+            end
+            if r>black_limit
+                f(i,j) = 255;
+            end
+        end
+    end
+    disp(size(img))
+    disp(size(f))
+    uint8(f)
+end
+
+
+function f=gauss_noise_original(img, white_limit, black_limit, mu, sigma)
     [i,j] = size(img);
     for i=1:i
         for j=1:j
@@ -9,6 +37,7 @@ end
 
 function f=add_noise(pixel,white_limit,black_limit,  mu, sigma)
    r = normrnd(mu,sigma);
+   r = mu+(sigma-mu)*r;
    if r <= white_limit
        f=0;
        return
