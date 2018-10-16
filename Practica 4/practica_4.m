7894976f080aa90e425ce726c7bb52db4a9f6572
jnvillar <juaninolivillar@gmail.com>
2018-10-16 19:31:32 -0300

    practica 4

--- Practica 4/practica_4.m
+++ Practica 4/practica_4.m
@@ -4,7 +4,8 @@ addpath(strcat(filepath, '/../Practica 3'));
 test = imread('imgs/test.png');
 lena = imread('imgs/lena.png');
 
-show_2 = 0;
+show_2_a = 0;
+show_2_c = 1;
 
 % Ejercicio 1
 
@@ -28,7 +29,7 @@ test_r_noise_3 = rayleigh_noise(test,0, 3);
 
 % Ejercicio 2
 
-if show_2
+if show_2_a
 m = 2; % m must be close to 2 
 
 %-----------lena image-----------%
@@ -70,6 +71,64 @@ figure('Name','rayleigh noise 2'); imshow([test_r_noise_2, test_r_noise_2_border
 
 test_r_noise_3_borders = lapplacian_borders(test_r_noise_3,6000,m);
 figure('Name','rayleigh noise 3'); imshow([test_r_noise_3, test_r_noise_3_borders])
+end
+
+if show_2_c
+m = 2; % m must be close to 2 
+
+%-----------lena image-----------%
+
+lena_g_noise_1 = smoothing(lena_g_noise_1);
+lena_g_noise_2 = smoothing(lena_g_noise_2);
+lena_g_noise_3 = smoothing(lena_g_noise_3);
+lena_r_noise_1 = smoothing(lena_r_noise_1);
+lena_r_noise_2 = smoothing(lena_r_noise_2);
+lena_r_noise_3 = smoothing(lena_r_noise_3);
+
+lena_g_noise_1_borders = lapplacian_borders(lena_g_noise_1,500,m);
+figure('Name','Gauss noise 0.1, with gauss blur'); imshow([lena_g_noise_1, lena_g_noise_1_borders])
+
+lena_g_noise_2_borders = lapplacian_borders(lena_g_noise_2,600,m);
+figure('Name','Gauss noise 0.5, with gauss blur'); imshow([lena_g_noise_2, lena_g_noise_2_borders])
+
+lena_g_noise_3_borders = lapplacian_borders(lena_g_noise_3,650,m);
+figure('Name','Gauss noise 0.9, with gauss blur'); imshow([lena_g_noise_3, lena_g_noise_3_borders])
+
+lena_r_noise_1_borders = lapplacian_borders(lena_r_noise_1,500,m);
+figure('Name','rayleigh noise 1, with gauss blur'); imshow([lena_r_noise_1, lena_r_noise_1_borders])
+
+lena_r_noise_2_borders = lapplacian_borders(lena_r_noise_2,500,m);
+figure('Name','rayleigh noise 2, with gauss blur'); imshow([lena_r_noise_2, lena_r_noise_2_borders])
+
+lena_r_noise_3_borders = lapplacian_borders(lena_r_noise_3,500,m);
+figure('Name','rayleigh noise 3, with gauss blur'); imshow([lena_r_noise_3, lena_r_noise_3_borders])
+
+%-----------test image-----------%
+
+test_g_noise_1 = smoothing(test_g_noise_1);
+test_g_noise_2 = smoothing(test_g_noise_2);
+test_g_noise_3 = smoothing(test_g_noise_3);
+test_r_noise_1 = smoothing(test_r_noise_1);
+test_r_noise_2 = smoothing(test_r_noise_2);
+test_r_noise_3 = smoothing(test_r_noise_3);
+
+test_g_noise_1_borders = lapplacian_borders(test_g_noise_1,500,m);
+figure('Name','Gauss noise 0.1 with gauss blur'); imshow([test_g_noise_1, test_g_noise_1_borders])
+
+test_g_noise_2_borders = lapplacian_borders(test_g_noise_2,600,m);
+figure('Name','Gauss noise 0.5 with gauss blur'); imshow([test_g_noise_2, test_g_noise_2_borders])
+
+test_g_noise_3_borders = lapplacian_borders(test_g_noise_3,700,m);
+figure('Name','Gauss noise 0.9 with gauss blur'); imshow([test_g_noise_3, test_g_noise_3_borders])
+
+test_r_noise_1_borders = lapplacian_borders(test_r_noise_1,500,m);
+figure('Name','rayleigh noise 1 with gauss blur'); imshow([test_r_noise_1, test_r_noise_1_borders])
+
+test_r_noise_2_borders = lapplacian_borders(test_r_noise_2,500,m);
+figure('Name','rayleigh noise 2 with gauss blur'); imshow([test_r_noise_2, test_r_noise_2_borders])
+
+test_r_noise_3_borders = lapplacian_borders(test_r_noise_3,500,m);
+figure('Name','rayleigh noise 3 with gauss blur'); imshow([test_r_noise_3, test_r_noise_3_borders])
 end
 
 
