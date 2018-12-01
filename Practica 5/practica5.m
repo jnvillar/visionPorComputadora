2f60a3ece8b5fc0cdccd1233b213495a8a7a3d51
ax-lew <axel.lew@hotmail.com>
2018-12-01 19:07:49 -0300

    fix dlt?

--- Practica 5/practica5.m
+++ Practica 5/practica5.m
@@ -2,6 +2,8 @@ filepath = fileparts(mfilename('fullpath'));
 addpath(strcat(filepath, '/../imgs'));
 addpath(strcat(filepath, '/../Practica 3'));
 
+
+
 lena = imread('imgs/lena.png');
 img_a = rgb2gray(imread('imgs/damero1.jpg'));
 img_b = rgb2gray(imread('imgs/damero2.jpg'));
@@ -9,44 +11,14 @@ img_b = rgb2gray(imread('imgs/damero2.jpg'));
 scale = 0.125;
 img_a = imresize(img_a,scale);
 img_b = imresize(img_b,scale);
-size(img_a)
 
 edges_img_a = scale*[ 870 780; 1167 792; 869 1085; 1162 1090];
-edges_img_b = scale*[ 285 1832 ; 409 1617; 482 1960; 612 1739];
+edges_img_b = scale*[612 1739;482 1960; 409 1617; 285 1832];
+
 [edges, ignore] = size(edges_img_a);
 
 
 
-
-
-% img_b = new_img; 
-% edges_img_b = h2*[edges_img_a'; ones(1,length(edges_img_a))];
-% edges_img_b = edges_img_b'
-% 
-% offset = offsets(img_a, h2)
-% edges_img_b = edges_img_b - [repmat([offset 0],4,1)]
-
-
-
-
-% close all;
-% figure
-% imagesc(img_a)
-% colormap(gray)
-% hold on;
-% plot(edges_img_a(:,1),edges_img_a(:,2),'gx-')
-% title('image a ')
-% 
-% figure
-% imagesc(img_b)
-% colormap(gray)
-% hold on;
-% plot(edges_img_b(:,1),edges_img_b(:,2),'gx-')
-% title('image b ')
-
-
-
-%show_selected_edges(edges, img_a, img_b, edges_img_a, edges_img_b)
 corresponde_matrixes = zeros(2*edges, 9);
 
 for i=1:edges
@@ -60,16 +32,11 @@ for i=1:edges
 end
 
 
-disp(corresponde_matrixes)
-
 [U,S,V] = svd(corresponde_matrixes);
 h = V(:,9);
-h = vec2mat(h, 3);
+h = [h(1) h(2) h(3); h(4) h(5) h(6); h(7) h(8) h(9)];
 
 ang = pi/4;
-h = [cos(ang) -sin(ang) 0;
-    sin(ang) cos(ang) 0;
-    0 0 1];
 
 
 
@@ -77,6 +44,7 @@ new_img = res_image(img_a, h);
 offset = offsets(img_a, h);
 [width_original_img, height_original_img] = size(img_a);
 [width, height] = size(new_img);
+
 invh = inv(h);
 
 
@@ -86,13 +54,15 @@ for i=1:width
         if pos_in_original_img(1) <= 0 || pos_in_original_img(1) > width_original_img || pos_in_original_img(2) <= 0 || pos_in_original_img(2) > height_original_img
         
         else
-             new_img(i,j) = img_a(pos_in_original_img(1),pos_in_original_img(2));
+            new_img(i,j) = img_a(pos_in_original_img(1),pos_in_original_img(2));
         end
     end
 end
 
 
 figure; imshow(new_img);
+figure; imshow(img_a);
+figure; imshow(img_b);
 
 
 function f = offsets(img, h)
@@ -138,10 +108,10 @@ function show_selected_edges(img_a, img_b, edges_img_a, edges_img_b)
 end
 
 function f = transform(x,y,m)
+
     v = [x y 1]';
     v = m*v;
-    v = [round(v(1)/v(3)) round(v(2)/v(3))];
-    f = v;
+    f = [round(v(1)/v(3)) round(v(2)/v(3))];
 end
 
 
