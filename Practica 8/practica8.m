a90d6c1c70fa64156a395a7b67e8097099fedde9
jnvillar <juaninolivillar@gmail.com>
2018-12-11 19:21:42 -0300

    practica 8

---
+++ Practica 8/practica8.m
@@ -0,0 +1,35 @@
+% Ejercicio 1
+
+% a) φz = 90 φy = −90◦
+
+rz = [ cosd(90) -sind(90) 0        ; sind(90) cosd(90) 0; 0         0      1        ];
+ry = [ cosd(-90) 0         sind(-90); 0        1        0;-sind(-90) 0      cosd(-90)];
+
+res1 = rz*ry; 
+
+% b) φx = 90 φz = 90◦
+
+rx = [1         0         0       ; 0        cosd(90)  -sind(90);0   sind(90) cosd(90)];
+rz = [ cosd(90) -sind(90) 0        ; sind(90) cosd(90) 0; 0         0      1        ];
+
+res2 = rz*rx; 
+
+% c) φx = 45 φy = 90 φz = 30
+
+rx = [1         0       0       ; 0        cosd(45) -sind(45); 0         sind(45) cosd(45)];
+rz = [cosd(30) -sin(30) 0       ; sind(30) cosd(30) 0        ; 0         0        1];
+ry = [cosd(90) 0        sind(90); 0        1        0        ; -sind(90) 0        cosd(90)];
+
+res3 = rz*ry*rx; 
+
+% Ejercicio 2
+
+x = [3 4 -3];
+c = [5, 3, 5];
+ry = [ cosd(-90) 0         sind(-90); 0        1        0;-sind(-90) 0      cosd(-90)];
+
+xcam = ry*(x-c)';
+
+% Ejercicio 3
+
+
