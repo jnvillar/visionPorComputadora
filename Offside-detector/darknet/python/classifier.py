cd90cdfc16be07cb5bd78fc8bc1f81b41e38775e
ax-lew <axel.lew@hotmail.com>
2019-04-19 11:23:25 -0300

    changes

--- Offside-detector/darknet/python/classifier.py
+++ Offside-detector/darknet/python/classifier.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
 from matplotlib import pyplot as plt
 class Classifier:
 
@@ -13,6 +14,17 @@ class Classifier:
     def classify(self, bounding_box, frame):
         player_histogram = self.calculate_histogram(frame, bounding_box)
         self.player_histograms.append(player_histogram)
+
+    def _is_grass(self, pixel):
+        rgbColor = np.uint8([[[0,255,0]]])
+        hsvColor = cv2.cvtColor(rgbColor, cv2.COLOR_BGR2HSV)
+        sensitivity = 20;
+        COLOR_MIN = np.array([hsvColor[0][0][0] - sensitivity, 100, 70])
+        COLOR_MAX = np.array([hsvColor[0][0][0] + sensitivity, 255, 255])
+
+        
+        pixel = cv2.cvtColor(np.uint8([[[pixel[0],pixel[1],pixel[2]]]]), cv2.COLOR_BGR2HSV)
+        return COLOR_MIN[0] <= pixel[0][0][0] <= COLOR_MAX[0]
 
     def calculate_mask(self, frame, bounding_box):
         mask = np.zeros(frame.shape[:2], np.uint8)
@@ -26,21 +38,24 @@ class Classifier:
         initial_y = int(y - (h / 2))
 
         mask[initial_y: initial_y + h, initial_x: initial_x + w] = 255
+        for j in range(initial_y, initial_y + h):
+            for i in range(initial_x, initial_x + w):
+                if self._is_grass(frame[j,i]):
+                    mask[j,i] = 0
         return mask
 
     def calculate_histogram(self, frame, bounding_box):
         mask = self.calculate_mask(frame, bounding_box)
+        res = cv2.bitwise_and(frame,frame,mask = mask)
+        #cv2.imshow('res', res)
+        #equipo = raw_input('continue')
+
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        h1 = cv2.calcHist([frame], [0], mask, [180], [0, 180])
-        h2 = cv2.calcHist([frame], [1], mask, [255], [0, 255])
-        h3 = cv2.calcHist([frame], [2], mask, [255], [0, 255])
-        return {'b': h1, 'g': h2, 'r': h3, 'bb': bounding_box}
+        h1 = cv2.calcHist([hsv_frame], [0], mask, [180], [0, 180])
+        return {'h': h1, 'bb': bounding_box}
 
     def histogram_distance(self, h1, h2):
-        diff = 0
-        diff += cv2.compareHist(h1['r'], h2['r'], method=cv2.HISTCMP_INTERSECT)
-        diff += cv2.compareHist(h1['g'], h2['g'], method=cv2.HISTCMP_INTERSECT)
-        diff += cv2.compareHist(h1['b'], h2['b'], method=cv2.HISTCMP_INTERSECT)
+        diff = cv2.compareHist(h1['h'], h2['h'], method=cv2.HISTCMP_INTERSECT)
         return diff
 
     def max_histogram_diff(self, histogram, players_histograms):
@@ -108,54 +123,59 @@ class Classifier:
         team_one, team_two = [], []
         
         for player in self.player_histograms:
-            player['attrs'] =  []
-            for col in ['g','b', 'r']:
-                player['attrs'].extend(player[col].ravel())
+            player['attrs'] = player['h'].ravel()
 
         m = [ 0,  6,  8,  9, 10]
         b = [ 1,  3,  4,  5,  7, 11, 12]
         r = [2,13]
-        
-        color = ('b','g','r')
-        for j,player in enumerate(self.player_histograms):
-                for i,col in enumerate(color):
-                    
-                    histr = player[col]
-                    plt.subplot(3,1,i+1)
-                    plt.plot(histr,color = col)
-                    if j in m:
-                        plt.title('player Madrid {}'.format(j))
-                    elif j in b:
-                        plt.title('player Barcelona {}'.format(j))
-                    else:
-                        plt.title('player Arbitro {}'.format(j))
-                    plt.xlim([0,256])
-                plt.show()
-                #cv2.waitKey(0)
-                #raw_input('continue?')
-        
 
+        for j,player in enumerate(self.player_histograms):
+                    
+                histr = player['h']
+
+                col = 'black' if j in m else 'blue' if j in b else 'g'
+                plt.subplot(len(self.player_histograms),1,j+1)
+                plt.plot(histr, color=col)
+
+                histr_1 = histr[:40]
+                histr_2 = histr[80:]
+                if j in m:
+                    plt.title('player Madrid {}'.format(j), loc='right')
+                    print('madrid', np.mean(histr_1), np.mean(histr_2))
+                    40-80
+                elif j in b:
+                    plt.title('player Barcelona {}'.format(j), loc='right')
+                    print('barcelona', np.mean(histr_1), np.mean(histr_2))
+                else:
+                    plt.title('player Arbitro {}'.format(j), loc='right')
+                    print('referi', np.mean(histr_1), np.mean(histr_2))
+                plt.xlim([0,180])
+        plt.show()
+        
+        self.player_histograms = [self.player_histograms[i] for i in range(len(self.player_histograms)) if i not in r]
         players_attributes = list(pd.DataFrame(self.player_histograms).attrs)
 
-        pca = PCA(n_components=5)
-        #import ipdb; ipdb.set_trace()
-        b = pca.fit_transform(np.array(players_attributes))
-        print(b)
-        print(pca.components_)
-        kmeans = KMeans(n_clusters=3, random_state=0).fit(b)
-        print(kmeans.labels_)
+       	#pca = PCA(n_components=2)
+        #pca_attrs = pca.fit_transform(np.array(players_attributes))
+        #plt.scatter(pca_attrs[:,0], pca_attrs[:,1], c=['black', 'b', 'b', 'b', 'b', 'black', 'b', 'black', 'black', 'black', 'b', 'b'])
+        #plt.show()
+        #kmeans_labels = GaussianMixture(n_components=2).fit_predict(pca_attrs)
+        
+        kmeans_labels = GaussianMixture(n_components=2).fit_predict(np.array(players_attributes))
+        
+        print(kmeans_labels)
 
-        for i in range(len(b)):
-            if kmeans.labels_[i] == 0:
+        for i in range(len(kmeans_labels)):
+            if kmeans_labels[i] == 0:
                 team_one.append(self.player_histograms[i])
-            elif kmeans.labels_[i] == 1:
+            elif kmeans_labels[i] == 1:
                 team_two.append(self.player_histograms[i])
         team_one = [player['bb'] for player in team_one]
         team_two = [player['bb'] for player in team_two]
-
-
         '''
+        
         players_bb = self.player_histograms
+        players_bb = [players_bb[i] for i in range(len(players_bb)) if i not in r]
         distances = self.calculate_distances(players_bb)
 
         players_bb = self.delete_referees(referee, distances, players_bb)
@@ -181,3 +201,5 @@ class Classifier:
         team_two = [player['bb'] for player in team_two]
         '''
         return team_one, team_two
+
+
